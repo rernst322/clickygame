e446664243765ffd2942cb040326333046543ec9
Renee Ernst <reneeErnst322@gmail.com>
2018-03-14 23:48:46 -0500

    Updates

--- my-app/src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Footer.css";
-
-const Footer = () =>
-    <footer className="footer">
-        <span>Clicky Game 2018</span>
-    </footer>;
-
-export default Footer;
